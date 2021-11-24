95887c57eff7f7bbe07e9e02a7c7d53766678ddb
Rafael Rotiliano <rafaelrotiliano.89@gmail.com>
2021-11-24 20:33:10 -0300

    Atualizaçao Views-Usuarios

--- paginas/views.py
+++ paginas/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class PaginaInicial(TemplateView):
-    template_name = "usuario/login2.html"
+    template_name = "usuarios/login2.html"
 
 class MenuView(TemplateView):
     template_name = "paginas/menu.html"
